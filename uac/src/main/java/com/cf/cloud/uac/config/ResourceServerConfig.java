e1bb5629b0e1cc85c4f8e165776dde65e257c683
cpazstido <120964417@qq.com>
2018-09-21 16:48:11 +0800

    Initial commit

---
+++ uac/src/main/java/com/cf/cloud/uac/config/ResourceServerConfig.java
@@ -0,0 +1,36 @@
+package com.cf.cloud.uac.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+
+import javax.servlet.http.HttpServletResponse;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig  extends ResourceServerConfigurerAdapter {
+    @Autowired
+    private CustomAccessDeniedHandler customAccessDeniedHandler;
+    @Autowired
+    private CustomHttp403ForbiddenEntryPoint customHttp403ForbiddenEntryPoint;
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .headers().frameOptions().disable()
+                .and()
+                .csrf().disable()
+                .exceptionHandling()
+                .authenticationEntryPoint((request, response, authException) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED))
+                .and()
+                .authorizeRequests().antMatchers("/pay/alipayCallback", "/druid/**", "/swagger-ui.html", "/swagger-resources/**", "/v2/api-docs", "/api/applications").permitAll()
+                .anyRequest().authenticated()
+                .and().exceptionHandling().authenticationEntryPoint(customHttp403ForbiddenEntryPoint)
+                .and().exceptionHandling().accessDeniedHandler(customAccessDeniedHandler)
+        ;
+    }
+}
