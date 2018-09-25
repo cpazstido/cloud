1fdfa07f04c01ec44783312e117b30b6962daf4c
cpazstido <120964417@qq.com>
2018-09-25 14:35:39 +0800

    f

--- oauth-server/src/main/java/com/cf/cloud/oauth/config/AuthenticationBeanConfig.java
+++ oauth-server/src/main/java/com/cf/cloud/oauth/config/AuthenticationBeanConfig.java
@@ -3,13 +3,18 @@ package com.cf.cloud.oauth.config;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class AuthenticationBeanConfig {
     @Bean
     @ConditionalOnMissingBean(UserDetailsService.class)
     public UserDetailsService userDetailsService() {
-        return new DefaultUserDetailsServiceImpl();
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("admin").password("admin").authorities("admin").build());
+        manager.createUser(User.withUsername("user").password("user").authorities("USER").build());
+        return manager;
     }
 }
